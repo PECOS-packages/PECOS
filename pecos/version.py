f215c141ac2e9bf9f5516ac598c91f485c52e774
Ciaran Ryan-Anderson <ciaranra@gmail.com>
2019-01-30 14:51:59 +0000

    Updated version number to 0.1.2

--- pecos/version.py
+++ pecos/version.py
@@ -18,7 +18,7 @@
 #   limitations under the License.
 #  =========================================================================  #
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Ciarán Ryan-Anderson'
 __copyright__ = '2018 National Technology & Engineering Solutions of Sandia, LLC (NTESS). Under the terms ' \
                 'of Contract DE-NA0003525 with NTESS, the U.S. Government retains certain rights in this software'

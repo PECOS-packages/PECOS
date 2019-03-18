b93ac4983c8e1acb42dc453e15933f2777e899c1
Ciaran Ryan-Anderson <ciaranra@gmail.com>
2019-03-18 18:19:58 +0000

    Simplifying the `logical_sign` method

--- pecos/simulators/_projectq_wrapper/logical_sign.py
+++ pecos/simulators/_projectq_wrapper/logical_sign.py
@@ -1,16 +1,14 @@
-from typing import Optional
 from ...circuits import QuantumCircuit
 
+
 def find_logical_signs(state,
-                       logical_circuit: QuantumCircuit,
-                       delogical_circuit: Optional[QuantumCircuit] = None) -> int:
+                       logical_circuit: QuantumCircuit) -> int:
     """
     Find the sign of the logical operator.
 
     Args:
         state:
         logical_circuit:
-        delogical_circuit:
 
     Returns:
 
@@ -18,14 +16,8 @@ def find_logical_signs(state,
     if len(logical_circuit) != 1:
         raise Exception('Logical operators are expected to only have one tick.')
 
-    if delogical_circuit and len(delogical_circuit) != 1:
-        raise Exception('Delogical operators are expected to only have one tick.')
-
     logical_xs = set([])
     logical_zs = set([])
-
-    delogical_xs = set([])
-    delogical_zs = set([])
 
     op_string = []
 
@@ -46,29 +38,6 @@ def find_logical_signs(state,
                 op_string.append('Y%s' % loc)
         else:
             raise Exception('Can not currently handle logical operator with operator "%s"!' % symbol)
-
-    if delogical_circuit:  # Check the relationship between logical operator and delogical operator.
-        for symbol, gate_locations, _ in delogical_circuit.items():
-
-            if symbol == 'X':
-                delogical_xs.update(gate_locations)
-            elif symbol == 'Z':
-                delogical_zs.update(gate_locations)
-            elif symbol == 'Y':
-                delogical_xs.update(gate_locations)
-                delogical_zs.update(gate_locations)
-            else:
-                raise Exception('Can not currently handle logical operator with operator "%s"!' % symbol)
-
-        # Make sure the logical and delogical anti-commute
-
-        anticom_x = len(logical_xs & delogical_zs) % 2  # Number of common elements modulo 2
-        anticom_z = len(logical_zs & delogical_xs) % 2  # Number of common elements modulo 2
-
-        if not ((anticom_x + anticom_z) % 2):
-            print('logical Xs: %s logical Zs: %s' % (logical_xs, logical_zs))
-            print('delogical Xs: %s delogical Zs: %s' % (delogical_xs, delogical_zs))
-            raise Exception("Logical and delogical operators supplied do not anti-commute!")
 
     op_string = ' '.join(op_string)
     result = state.eng.backend.get_expectation_value(op_string, state.self.qureg)

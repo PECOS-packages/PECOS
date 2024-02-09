060f73365510a3f42b50065a448e97c9a03e0550
Ciaran Ryan-Anderson <ciaran.ryan-anderson@quantinuum.com>
2024-02-09 09:22:06 -0700

    Adding physical gates needed to build up circuits.

---
+++ python/pecos/slr/phys/misc.py
@@ -0,0 +1,17 @@
+from .metaclasses import SingleQubitUnitary
+
+
+class TGate(SingleQubitUnitary): ...
+
+
+T = TGate(qasm_sym="rz(pi/4)")
+FZ = T
+
+
+class TdgGate(SingleQubitUnitary): ...
+
+
+Tdg = TdgGate(qasm_sym="rz(-pi/4)")
+FZdg = Tdg
+
+# TODO: SWAP, iSWAP, Toffoli (CCNOT, CCX, TOFF)

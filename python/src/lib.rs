bf5699efe7a2232bec26f8dfd8577f3e9ce3ca5c
Ciaran Ryan-Anderson <ciaranra@gmail.com>
2024-02-11 00:13:24 -0700

    Moved sim code to sim.rs for pyo3pecos

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,39 +1,9 @@
-extern crate pecos_sim;
+mod sim;
 
-use pecos_sim::CoinToss;
 use pyo3::prelude::*;
-
-#[pyclass]
-struct PyCoinToss {
-    inner: CoinToss,
-}
-
-#[pymethods]
-impl PyCoinToss {
-    #[new]
-    fn new(prob: f64, num_qubits: usize) -> Self {
-        PyCoinToss {
-            inner: CoinToss::new(num_qubits, prob),
-        }
-    }
-
-    fn h(&mut self, qubit: usize) {
-        self.inner.h(qubit)
-    }
-
-    fn meas(&mut self, qubit: usize) -> PyResult<bool> {
-        Ok(self.inner.meas(qubit))
-    }
-}
-
-#[pyfunction]
-fn add(a: usize, b: usize) -> PyResult<usize> {
-    Ok(pecos_sim::add(a, b))
-}
 
 #[pymodule]
 fn pecos_pyo3(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(add, m)?)?;
-    m.add_class::<PyCoinToss>()?;
+    m.add_class::<sim::PyCoinToss>()?;
     Ok(())
 }

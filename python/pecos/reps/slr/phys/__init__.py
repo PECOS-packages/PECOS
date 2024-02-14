619f1222cfad9aac27cbce1da342c27f3741d06c
Ciaran Ryan-Anderson <ciaran.ryan-anderson@quantinuum.com>
2024-02-13 18:08:40 -0700

    Adding slr code

---
+++ python/pecos/reps/slr/phys/__init__.py
@@ -0,0 +1,45 @@
+from pecos.slr.phys.cliffords_tq import CH, CX, CY, CZ, SXX, SYY, SZZ, SXXdg, SYYdg, SZZdg
+from pecos.slr.phys.face_rots import F4, F, F4dg, Fdg
+from pecos.slr.phys.hadamards import H
+from pecos.slr.phys.misc import FZ, FZdg, T, Tdg
+from pecos.slr.phys.paulis import X, Y, Z
+from pecos.slr.phys.projective import Measure, Reset
+from pecos.slr.phys.rots import RX, RY, RZ, RZZ
+from pecos.slr.phys.sqrt_paulis import SX, SY, SZ, SXdg, SYdg, SZdg
+
+__all__ = [
+    "Measure",
+    "Reset",
+    "RX",
+    "RY",
+    "RZ",
+    "RZZ",
+    "X",
+    "Y",
+    "Z",
+    "SX",
+    "SY",
+    "SZ",
+    "SXdg",
+    "SYdg",
+    "SZdg",
+    "F",
+    "Fdg",
+    "F4",
+    "F4dg",
+    "H",
+    "CX",
+    "CY",
+    "CZ",
+    "SXX",
+    "SYY",
+    "SZZ",
+    "SXXdg",
+    "SYYdg",
+    "SZZdg",
+    "CH",
+    "T",
+    "Tdg",
+    "FZ",
+    "FZdg",
+]
---
+++ python/pecos/reps/slr/phys/hadamards.py
@@ -0,0 +1,8 @@
+from pecos.slr.phys.metaclasses import SQCliffordGate
+
+
+class HGate(SQCliffordGate):
+    ...
+
+
+H = HGate(qasm_sym="h")

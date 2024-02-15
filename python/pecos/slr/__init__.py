5b4b6ede2fa824630b3fa064c75cc7100890b1c2
Ciaran Ryan-Anderson <ciaranra@gmail.com>
2024-02-15 14:40:52 -0700

    Moving slr down to reduce nesting

--- python/pecos/slr/__init__.py
+++ python/pecos/slr/__init__.py
@@ -9,8 +9,7 @@
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-from pecos import __version__
-from pecos.reps.slr import phys
+from pecos.slr import phys
 from pecos.slr.block import Block
 from pecos.slr.cond_block import If, Repeat
 from pecos.slr.cops import Assign

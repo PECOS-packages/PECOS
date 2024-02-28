e6c16a5e7d22af125a06bb91eb3e267bad19dd40
PabloAndresCQ <pablo.andres-martinez@cambridgequantum.com>
2024-02-28 10:44:04 +0000

    Added copyright information

--- python/pecos/simulators/custatevec/state.py
+++ python/pecos/simulators/custatevec/state.py
@@ -1,4 +1,13 @@
-# TODO: Include license information?
+# Copyright 2023 The PECOS Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License.You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
 
 import random
 
@@ -33,7 +42,6 @@ class CuStateVec(StateVector):
         random.seed(seed)
 
         super().__init__()
-        # cusv.logger_set_level(5)  # Remove! here for debugging
 
         self.bindings = bindings.gate_dict
         self.num_qubits = num_qubits

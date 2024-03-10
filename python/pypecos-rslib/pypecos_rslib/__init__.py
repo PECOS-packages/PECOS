746af26d68b8b793c613d7c119bd74ee787ba7fd
Ciaran Ryan-Anderson <ciaranra@gmail.com>
2024-03-09 18:01:30 -0700

    Separating out a pypecos-rslib package.

--- python/pypecos-rslib/pypecos_rslib/__init__.py
+++ python/pypecos-rslib/pypecos_rslib/__init__.py
@@ -1,6 +1,4 @@
 # Copyright 2018 The PECOS Developers
-# Copyright 2018 National Technology & Engineering Solutions of Sandia, LLC (NTESS). Under the terms of Contract
-# DE-NA0003525 with NTESS, the U.S. Government retains certain rights in this software.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
 # the License.You may obtain a copy of the License at
@@ -21,45 +19,13 @@ __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("pypecos")
+    __version__ = version("pypecos-rslib")
 except PackageNotFoundError:
     __version__ = "0.0.0"
 
-# PECOS namespaces
-from pypecos import (
-    circuit_converters,
-    circuits,
-    decoders,
-    engines,
-    error_models,
-    misc,
-    qeccs,
-    simulators,
-    slr,
-    tools,
-)
-from pypecos.circuits.quantum_circuit import QuantumCircuit
-from pypecos.engines import circuit_runners
-from pypecos.engines.cvm.binarray import BinArray
-from pypecos.engines.hybrid_engine_old import HybridEngine
-
-from . import pyo3pecos as rs  # noqa: TID252
+from . import rslib  # noqa: TID252
 
 __all__ = [
     "__version__",
-    "rs",
-    "circuits",
-    "qeccs",
-    "simulators",
-    "error_models",
-    "circuit_runners",
-    "engines",
-    "decoders",
-    "circuit_converters",
-    "misc",
-    "tools",
-    "QuantumCircuit",
-    "BinArray",
-    "HybridEngine",
-    "slr",
+    "rslib",
 ]

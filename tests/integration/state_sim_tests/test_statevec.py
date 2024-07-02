05fb01734931a9b6b41f5f4d137d9f78a24fa8e5
PabloAndresCQ <pablo.andresmartinez@quantinuum.com>
2024-07-02 16:20:04 +0100

    Adding tests for HybridEngine on statevector simulators

--- tests/integration/state_sim_tests/test_statevec.py
+++ tests/integration/state_sim_tests/test_statevec.py
@@ -11,12 +11,23 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Callable
+
+    from pecos.simulators.sim_class_types import StateVector
+
+import json
 from importlib.metadata import version
+from pathlib import Path
 
 import numpy as np
 import pytest
 from packaging.version import parse as vparse
 from pecos.circuits import QuantumCircuit
+from pecos.engines.hybrid_engine import HybridEngine
+from pecos.error_models.generic_error_model import GenericErrorModel
 from pecos.simulators import BasicSV
 
 # Try to import the requirements for Qulacs
@@ -50,33 +61,30 @@ except (ImportError, AttributeError):
     custatevec_ready = False
 
 
-def verify(simulator, qc: QuantumCircuit, final_vector: np.ndarray) -> None:
+def check_dependencies(simulator) -> Callable[[int], StateVector]:
     if simulator == "BasicSV":
-        sim = BasicSV(len(qc.qudits))
+        sim = BasicSV
     elif simulator == "Qulacs" and qulacs_ready:
-        sim = Qulacs(len(qc.qudits))
+        sim = Qulacs
     elif simulator == "QuEST" and quest_ready:
-        sim = QuEST(len(qc.qudits))
+        sim = QuEST
     elif simulator == "CuStateVec" and custatevec_ready:
-        sim = CuStateVec(len(qc.qudits))
+        sim = CuStateVec
     else:
         pytest.skip(f"Requirements to test {simulator} are not met.")
+
+    return sim
+
+
+def verify(simulator, qc: QuantumCircuit, final_vector: np.ndarray) -> None:
+    sim = check_dependencies(simulator)(len(qc.qudits))
 
     sim.run_circuit(qc)
     assert np.allclose(sim.vector, final_vector)
 
 
 def check_measurement(simulator, qc: QuantumCircuit, final_results: dict[int, int] | None = None) -> None:
-    if simulator == "BasicSV":
-        sim = BasicSV(len(qc.qudits))
-    elif simulator == "Qulacs" and qulacs_ready:
-        sim = Qulacs(len(qc.qudits))
-    elif simulator == "QuEST" and quest_ready:
-        sim = QuEST(len(qc.qudits))
-    elif simulator == "CuStateVec" and custatevec_ready:
-        sim = CuStateVec(len(qc.qudits))
-    else:
-        pytest.skip(f"Requirements to test {simulator} are not met.")
+    sim = check_dependencies(simulator)(len(qc.qudits))
 
     results = sim.run_circuit(qc)
 
@@ -323,3 +331,63 @@ def test_all_gate_circ(simulator):
         # Measure
         qc.append({"Measure": {0, 1, 2, 3, 4}})
         check_measurement(simulator, qc)
+
+
+@pytest.mark.parametrize(
+    "simulator",
+    [
+        "Qulacs",
+        "QuEST",
+        "CuStateVec",
+    ],
+)
+def test_hybrid_engine_no_noise(simulator):
+    """Test that HybridEngine can use these simulators"""
+    check_dependencies(simulator)
+
+    n_shots = 1000
+    phir_folder = Path(__file__).parent.parent / "phir"
+
+    results = HybridEngine(qsim=simulator).run(
+        program=json.load(Path.open(phir_folder / "bell_qparallel.json")),
+        shots=n_shots,
+    )
+
+    m = results["m"]
+    assert np.isclose(m.count("00") / n_shots, m.count("11") / n_shots, atol=0.1)
+
+
+@pytest.mark.parametrize(
+    "simulator",
+    [
+        "Qulacs",
+        "QuEST",
+        "CuStateVec",
+    ],
+)
+def test_hybrid_engine_noisy(simulator):
+    """Test that HybridEngine with noise can use these simulators"""
+    check_dependencies(simulator)
+
+    n_shots = 1000
+    phir_folder = Path(__file__).parent.parent / "phir"
+
+    generic_errors = GenericErrorModel(
+        error_params={
+            "p1": 2e-1,
+            "p2": 2e-1,
+            "p_meas": 2e-1,
+            "p_init": 1e-1,
+            "p1_error_model": {
+                "X": 0.25,
+                "Y": 0.25,
+                "Z": 0.25,
+                "L": 0.25,
+            },
+        },
+    )
+    sim = HybridEngine(error_model=generic_errors)
+    sim.run(
+        program=json.load(Path.open(phir_folder / "example1_no_wasm.json")),
+        shots=n_shots,
+    )

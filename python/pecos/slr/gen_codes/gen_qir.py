a2a3cb15ae2cd5476a8d2bd543ba59917cb40c39
Pete Campora <petecampora@gmail.com>
2024-10-22 16:23:00 -0600

    Measure supported

--- python/pecos/slr/gen_codes/gen_qir.py
+++ python/pecos/slr/gen_codes/gen_qir.py
@@ -15,11 +15,12 @@ from collections import OrderedDict
 from dataclasses import dataclass
 from functools import cached_property
 
-from llvmlite import ir
-from llvmlite.ir import IntType, PointerType, VoidType
+from llvmlite import binding, ir
+from llvmlite.ir import IntType, DoubleType, PointerType, Type, VoidType
 
 from pecos import __version__
 from pecos.qeclib.qubit import qgate_base
+from pecos.qeclib.qubit.measures import Measure
 from pecos.qeclib.qubit.sq_hadamards import H
 from pecos.qeclib.qubit.tq_cliffords import CX
 from pecos.slr import Block, Main
@@ -27,86 +28,161 @@ from pecos.slr.gen_codes.generator import Generator
 from pecos.slr.misc import Barrier, Comment, Permute
 from pecos.slr.vars import CReg, QReg, Reg, Vars, Qubit
 
-
-@dataclass
 class QIRTypes:
-    """Dataclass to hold the types used in QIR compilation"""
+    """Class to hold the types used in QIR compilation"""
 
-    boolType: IntType
-    intType: IntType
-    qubitPtrType: PointerType
-    resultPtrType: PointerType
+    def __init__(self, module: ir.Module):
+        # Create some useful types to use in compilation later
+        qubitTy = module.context.get_identified_type("Qubit")
+        resultTy = module.context.get_identified_type("Result")
+        self.voidType: VoidType = ir.VoidType()
+        self.boolType: IntType = ir.IntType(1)
+        self.intType: IntType = ir.IntType(64)
+        self.doubleType: DoubleType = ir.DoubleType()
+        self.qubitPtrType: PointerType = qubitTy.as_pointer()
+        self.resultPtrType: PointerType = resultTy.as_pointer()        
+
+
+class QIRFunc:
+    """Represents a callable function in a QIR program"""
+    
+    def __init__(self, module: ir.Module, ret_ty: Type, arg_tys: list[Type], name: str):
+        self.binding = ir.Function(
+            module,
+            ir.FunctionType(ret_ty, arg_tys),
+            name=name,
+        )
+
+    def call(self, builder: ir.IRBuilder, args: list[binding.ValueRef], name: str) -> binding.ValueRef:
+        """A helper method to call a QIR Gate"""
+        return builder.call(self.binding, args, name)
+
+    def __repr__(self) -> str:
+        return self.name
+        
+    
+class QIRGate(QIRFunc):
+    """Represents a quantum gate in QIR"""
+    
+    def __init__(self, module: ir.Module, arg_tys: list[Type], name: str):
+        self._arg_tys = arg_tys
+        self._mangled_name: str = "__quantum__qis__" + name + "__body"
+        self._name:str = name
+        super().__init__(module, ir.VoidType(), arg_tys, self._mangled_name)
+
+    @property
+    def mangled_name(self) -> str:
+        return self._mangled_name
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def llvm_type_str(self) -> str:
+        """returns the llvm typeas a string"""
+        
+        return f'void @{self_mangle_name}({", ".join(map(str, self._arg_tys))})'
+
+        
+class QIRGates:
+    """A collection of QIR gates."""
+    
+    def __init__(self, module: ir.Module, types: QIRTypes):
+        self.h_func = QIRGate(
+            module,
+            [types.qubitPtrType],
+            name="h",
+        )
+        self.cx_func = QIRGate(
+            module,
+            [types.qubitPtrType, types.qubitPtrType],
+            name="cx",
+        )
+
+        
+class CRegFuncs:
+    """A collection of QIR Functions that aren't gates"""
+    
+    def __init__(self, module: ir.Module, types: QIRTypes):
+        self.create_creg_func = QIRFunc(
+            module,
+            types.boolType.as_pointer(),
+            [types.intType],
+            "create_creg",
+        )
+    #TODO: add functions to set and read bits in a creg and also conver to an integer
+
+    
+class MzToBit(QIRFunc):
+    """Represents a QIR measure call in the Z basis that writes to a bit in a creg"""
+    
+    def __init__(self, module: ir.Module, types: QIRTypes):
+        super().__init__(module,
+                         types.voidType,
+                         [types.qubitPtrType, types.boolType.as_pointer(), types.intType],
+                         "mz_to_creg_bit")
 
 
 class QIRGenerator(Generator):
     """Class to generate QIR from SLR. This should enable better compilation of conditional programs."""
 
     def __init__(self, includes: list[str] | None = None):
-        # self.output = []
         self.current_block: Block = None
-        # self.includes = includes
-        # self.cond = None
         self.setup_module()
         # Create a field qreg_list
-        self.qreg_dict: dict[str, tuple[int, int]] = OrderedDict()
+        self._qreg_dict: dict[str, tuple[int, int]] = OrderedDict()
         self._qubit_count: int = 0
-        self._creg: dict[str, object] = {}  # replace 'object' with the correct type
+        self._creg_dict: dict[str, object] = {}  # replace 'object' with the correct type
 
     def setup_module(self):
-        self._module = ir.Module(name=__file__)
+        self._module = ir.Module(name=__file__)        
 
-        # Create some useful types to use in compilation later
-        boolTy = ir.IntType(1)
-        intTy = ir.IntType(64)
-        qubitTy = self._module.context.get_identified_type("Qubit")
-        resultTy = self._module.context.get_identified_type("Result")
-        qubitPtrTy = qubitTy.as_pointer()
-        resultPtrTy = resultTy.as_pointer()
+        # store them in a read-only object
+        self._types = QIRTypes(self._module)
 
-        self._types = QIRTypes(boolTy, intTy, qubitPtrTy, resultPtrTy)
-        fnty = ir.FunctionType(VoidType(), [])
+        # setup the quantum gates to be used in compilation
+        self._gates = QIRGates(self._module, self._types)
 
-        self._main_func = ir.Function(self._module, fnty, name="main")
+        # setup the measurement function to be used
+        self._mz_to_bit = MzToBit(self._module, self._types)
 
-        self.create_creg_func = ir.Function(
-            self._module,
-            ir.FunctionType(boolTy.as_pointer(), [intTy]),
-            name="create_creg",
-        )
+        # setup functions to manipulate cregs
+        self._creg_funcs = CRegFuncs(self._module, self._types)
 
-        self.mz_to_creg_bit_func = ir.Function(
-            self._module,
-            ir.FunctionType(VoidType, qubitPtrType, [boolTy.as_pointer(), intTy]),
-            name="mz_to_creg_bit"
-        )
+        # declare the main function
+        main_fnty = ir.FunctionType(self._types.voidType, [])
+        self._main_func = ir.Function(self._module, main_fnty, name="main")        
 
         # Now implement the function
         self.entry_block = self._main_func.append_basic_block(name="entry")
         self.current_block = self.entry_block
-        self.builder = ir.IRBuilder(self.entry_block)
-        self.builder.comment(f"// Generated using: PECOS version {__version__}")
+        self._builder = ir.IRBuilder(self.entry_block)
+        self._builder.comment(f"// Generated using: PECOS version {__version__}")
 
     def create_creg(self, creg: CReg):
         """Add a call to create_creg in the current block."""
-        self._creg[creg.sym] = self.builder.call(
-            self.create_creg_func,
+
+        
+        self._creg_dict[creg.sym] = self._creg_funcs.create_creg_func.call(
+            self._builder,
             [ir.Constant(ir.IntType(64), creg.size)],
-            name=f"{creg.sym}",
+            f"{creg.sym}",
         )
 
     def create_qreg(self, qreg: QReg):
         """Uses an OrderedDict to globally flatten quantum registers into a single global register."""
-        self.qreg_dict[qreg.sym] = (self._qubit_count, self._qubit_count + qreg.size - 1)
+        
+        self._qreg_dict[qreg.sym] = (self._qubit_count, self._qubit_count + qreg.size - 1)
         self._qubit_count += qreg.size
-        # return self.builder.call(self.create_creg_func, [ir.Constant(ir.IntType(64), creg.size)], name = f"{creg.sym}")
-
+        
     def generate_block(self, block: Main) -> None:
         """Primary entry point for generation. Processes an slr Main block."""
 
         self._handle_main_block(block)
         self._handle_block(block)
 
-        self.builder.ret_void()
+        self._builder.ret_void()
 
     def _handle_var(self, reg: Reg) -> None:
         match reg:
@@ -117,6 +193,7 @@ class QIRGenerator(Generator):
 
     def _handle_main_block(self, block) -> None:
         """Process the main block of the program."""
+        
         for var in block.vars:
             self._handle_var(var)
 
@@ -156,13 +233,13 @@ class QIRGenerator(Generator):
 
     def _handle_quantum_gate(self, gate: qgate_base.QGate) -> None:
         """Process a quantum gate."""
+        
         # Reminder: qgate has sym, qargs, and params properties
         match gate:
             case H():
-                self.builder.call(self._h_func, [self._qarg_to_qubit_ptr(gate.qargs[0])], name="")
+                self._gates.h_func.call(self._builder, [self._qarg_to_qubit_ptr(gate.qargs[0])], name="")
             case CX():
-                self.builder.call(
-                    self._cx_func,
+                    self._gates.cx_func.call(self._builder,
                     [
                         self._qarg_to_qubit_ptr(gate.qargs[0]),
                         self._qarg_to_qubit_ptr(gate.qargs[1]),
@@ -170,31 +247,23 @@ class QIRGenerator(Generator):
                     name="",
                 )
             case Measure():
-                self.mz_to_creg_bit
-
-    @cached_property
-    def _h_func(self) -> ir.Function:
-        return ir.Function(
-            self._module,
-            ir.FunctionType(VoidType(), [self._types.qubitPtrType]),
-            name="__quantum__qis__h__body",
-        )
-
-    @cached_property
-    def _cx_func(self) -> ir.Function:
-        return ir.Function(
-            self._module,
-            ir.FunctionType(VoidType(), [self._types.qubitPtrType, self._types.qubitPtrType]),
-            name="__quantum__qis__cnot__body",
-        )
+                creg = gate.cout[0]
+                ll_creg = self._creg_dict[creg.sym]
+                for i, q in enumerate(gate.qargs[0]):
+                    qubit_ptr = self._qarg_to_qubit_ptr(q)                    
+                    self._mz_to_bit.call(self._builder,
+                                        [qubit_ptr, ll_creg, ir.Constant(self._types.intType, i)],
+                                        name="")    
 
     def _qarg_to_qubit_ptr(self, qarg: Qubit):
         """Return a pointer to a qubit in the global register, based on the register and index
         passed in the `qarg` param."""
+        
         index = qarg.index
-        qubit_index = self.qreg_dict[qarg.reg.sym][0] + index
-        return self.builder.inttoptr(ir.Constant(ir.IntType(64), qubit_index), self._types.qubitPtrType)
+        qubit_index = self._qreg_dict[qarg.reg.sym][0] + index
+        return ir.Constant(self._types.intType, qubit_index).inttoptr(self._types.qubitPtrType)
 
     def get_output(self) -> str:
-        # This will stringify the module as ll via __repr__
+        """Stringify the module as .ll text"""
+        
         return str(self._module)

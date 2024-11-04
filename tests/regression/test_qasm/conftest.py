cbccecf3b294cb7d24f1efe8cdeaf32a81601532
Ciarán Ryan-Anderson <ciaranra@gmail.com>
2024-11-04 16:10:27 -0700

    bump v0.6.0.dev5 to v0.6.0.dev6 + update pre-commit + fix regression version issue (#88)

--- tests/regression/test_qasm/conftest.py
+++ tests/regression/test_qasm/conftest.py
@@ -5,10 +5,14 @@ from pathlib import Path
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture
 def compare_qasm():
-    def _compare_qasm(block, *params, directory: Path | None = None, filename: str | None = None):
-
+    def _compare_qasm(
+        block,
+        *params,
+        directory: Path | None = None,
+        filename: str | None = None,
+    ):
         if directory is None:
             directory = Path(__file__).parent
 
@@ -28,10 +32,13 @@ def compare_qasm():
 
         qasm1 = qasm1.strip()
 
-        if hasattr(block, "gen"):
+        # TODO: Fix this... this is kinda hacky
+        if hasattr(block, "qargs") and hasattr(block, "params") and hasattr(block, "sym"):
             qasm2 = block.gen("qasm").strip()
+        elif hasattr(block, "gen"):
+            qasm2 = block.gen("qasm", add_versions=False).strip()
         else:
-            qasm2 = block.qasm().strip()
+            qasm2 = block.qasm(add_versions=False).strip()
 
         assert qasm1 == qasm2
 

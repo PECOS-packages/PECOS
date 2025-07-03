9dd3f8d48d9fb9d55f2453e3068811d7f6f7f04e
Jonhas Colina <jonhas.colina@quantinuum.com>
2025-07-03 12:55:55 -0400

    pre-commit changes

--- clibs/pecos-rng/pecos_pcg/__init__.py
+++ clibs/pecos-rng/pecos_pcg/__init__.py
@@ -1,5 +1,10 @@
 """Python Package responsible for generating random numbers using pcg_rng."""
 
-from .pecos_rng import pcg32_boundedrand, pcg32_frandom, pcg32_random, pcg32_srandom
+from .pecos_rng import (
+    pcg32_boundedrand,
+    pcg32_frandom,
+    pcg32_random,
+    pcg32_srandom,
+)  # noqa: TID252
 
 __all__ = ["pcg32_boundedrand", "pcg32_frandom", "pcg32_random", "pcg32_srandom"]

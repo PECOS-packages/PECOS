f2f408bc0ac9eed62a9cba3cf18200635eb81b97
Ciaran Ryan-Anderson <ciaranra@gmail.com>
2026-03-13 07:57:44 -0600

    qasm to phir

--- crates/pecos-qasm/src/lib.rs
+++ crates/pecos-qasm/src/lib.rs
@@ -70,6 +70,9 @@ pub mod simulation;
 pub mod unified_engine_builder;
 pub mod util;
 
+#[cfg(feature = "phir")]
+pub mod qasm_to_phir;
+
 #[cfg(feature = "wasm")]
 pub mod wasm_foreign_object;
 
@@ -80,6 +83,8 @@ pub use engine_builder::QASMEngineBuilder;
 pub use parser::{ParseConfig, QASMParser};
 pub use preprocessor::Preprocessor;
 pub use program::QASMProgram;
+#[cfg(feature = "phir")]
+pub use qasm_to_phir::{qasm_program_to_phir_module, qasm_to_phir_module, qasm_to_ron};
 #[cfg(feature = "wasm")]
 pub use program::QasmEngineWasm;
 pub use unified_engine_builder::{QasmEngineBuilder, qasm_engine};
